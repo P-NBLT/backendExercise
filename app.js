9c44a58265c63bcffff4d7499e5ccbe800dff577
Pierre-Alexis Noblet <98712114+P-NBLT@users.noreply.github.com>
2022-10-05 13:25:16 +0200

    test with jest

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const app = require("express")();
+app.use(express.json());
+
+const agencies = [
+  { name: "Pits", city: "Amsterdam", id: 1 },
+  { name: "Marti", city: "Amsterdam", id: 2 },
+  { name: "Antic", city: "Rotterdam", id: 3 },
+];
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+app.get("/agencies", (req, res) => {
+  res.json(agencies);
+});
+
+app.post("/agencies", (req, res) => {
+  const agency = req.body;
+
+  agencies.push({ ...req.body });
+  return res.status(201).json({ success: true, agencies: agencies });
+});
+
+app.get("/agencies/:id", (req, res) => {
+  param = req.params;
+
+  const findAgency = agencies.find((agency) => agency.id == param.id);
+
+  res.status(200).json(findAgency);
+});
+
+app.patch("/agencies/:id", (req, res) => {
+  const param = req.params;
+  const body = req.body;
+  console.log("patch body", body, param);
+  const UpdateAgency = agencies.find((agency) => agency.id == param.id);
+  //   const differentValues = Object.agencies(UpdateAgency);
+  console.log("UpdateAgency before", UpdateAgency);
+  for (const [key, value] of Object.entries(UpdateAgency)) {
+    UpdateAgency[key] = !body[key]
+      ? (UpdateAgency[key] = value)
+      : (UpdateAgency[key] = body[key]);
+  }
+  console.log("UpdateAgency after", UpdateAgency);
+  res.status(200).json(UpdateAgency);
+});
+
+module.exports = app;
---
+++ app.test.js
@@ -0,0 +1,54 @@
+const app = require("./app");
+const request = require("supertest");
+const testApp = request(app);
+
+test("Read the text on the port", async () => {
+  const path = "/";
+
+  const response = await testApp.get("/");
+
+  expect(response.text).toBe("Hello");
+});
+
+test("Send agency to /agencies", async () => {
+  const path = "/agencies";
+  const newAgency = { name: "Pits", city: "Amsterdam" };
+  const response = await testApp.post(path).send(newAgency);
+
+  const agenciesRes = response.body;
+  const lastIndex = agenciesRes.agencies.length - 1;
+
+  expect(response.status).toBe(201);
+  expect(agenciesRes.success).toEqual(true);
+  expect(agenciesRes.agencies[lastIndex]).toEqual({
+    name: "Pits",
+    city: "Amsterdam",
+  });
+});
+
+test("Read agency at the id: index of the agencies array", async () => {
+  const agencies = [
+    { name: "Pits", city: "Amsterdam", id: 1 },
+    { name: "Marti", city: "Amsterdam", id: 2 },
+    { name: "Antic", city: "Rotterdam", id: 3 },
+  ];
+  const random = Math.floor(Math.random() * 3);
+  const id = agencies[random].id;
+  const agency = agencies.find((agency) => agency.id == id);
+  const response = await await testApp.get(`/agencies/${id}`);
+
+  expect(response.status).toEqual(200);
+  expect(response.body).toEqual(agency);
+});
+
+test("Modify agency at the id: index of the agencies array", async () => {
+  const modifiedAgency = { name: "Tech", city: "Rotterdam", id: 2 };
+  const path = "/agencies/2";
+
+  const response = await testApp.patch(path).send(modifiedAgency);
+
+  console.log(response.body);
+
+  expect(response.status).toBe(200);
+  expect(response.body).toEqual(modifiedAgency);
+});
